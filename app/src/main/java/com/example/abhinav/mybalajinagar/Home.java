0d9240acea576d1130e6f2603e57c365cf6c1cc1
amilaneni99 <as.abhi99@gmail.com>
2019-01-02 13:50:40 +0530

    Added Admin Messaging Service and Added UI Elements

--- app/src/main/java/com/example/abhinav/mybalajinagar/Home.java
+++ app/src/main/java/com/example/abhinav/mybalajinagar/Home.java
@@ -1,15 +1,32 @@
 package com.example.abhinav.mybalajinagar;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.HardwarePropertiesManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
@@ -18,10 +35,17 @@ public class Home extends AppCompatActivity {
     private ActionBarDrawerToggle t;
     private NavigationView nv;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        System.out.println("Home.onCreate: "+FirebaseInstanceId.getInstance().getInstanceId());
+
+        nv = findViewById(R.id.menu);
+
+        nv.setItemIconTintList(null);
+
 
         fop = findViewById(R.id.fop);
         messages = findViewById(R.id.notifications);
@@ -34,6 +58,11 @@ public class Home extends AppCompatActivity {
         t.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
 
         complaints.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +91,53 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(Home.this,Helplines.class));
             }
         });
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.messages1)
+                {
+                    Intent intent = new Intent(Home.this,Messages.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(id == R.id.helplines1)
+                {
+                    Intent intent = new Intent(Home.this,Helplines.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(id == R.id.fop1)
+                {
+                    Intent intent = new Intent(Home.this,FOP.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(id == R.id.complaints1)
+                {
+                    Intent intent = new Intent(Home.this,Complaints.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(id == R.id.events)
+                {
+                    Intent intent = new Intent(Home.this,Events.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(id == R.id.signout)
+                {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(Home.this,SignIn.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else {
+                    return true;
+                }
+            }
+        });
     }
 
     @Override
@@ -72,4 +148,5 @@ public class Home extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/abhinav/mybalajinagar/Messages.java
+++ app/src/main/java/com/example/abhinav/mybalajinagar/Messages.java
@@ -1,13 +1,76 @@
 package com.example.abhinav.mybalajinagar;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Messages extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private  DatabaseReference mMessagesDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private MessageAdapter mMessageAdapter;
+    private ListView mMessageListView;
+    List<Message> messages = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        mMessageListView = findViewById(R.id.messageListView);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        mMessagesDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                messages.clear();
+
+                mMessageAdapter = new MessageAdapter(Messages.this, R.layout.item_message, messages);
+                mMessageAdapter.notifyDataSetChanged();
+
+                for (DataSnapshot messagesSnapshot : dataSnapshot.getChildren()){
+                    Message message = messagesSnapshot.getValue(Message.class);
+                    messages.add(message);
+                }
+                mMessageAdapter.notifyDataSetChanged();
+                mMessageAdapter = new MessageAdapter(Messages.this, R.layout.item_message, messages);
+                mMessageListView.setAdapter(mMessageAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
